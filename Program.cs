0bfa5c6c5d775a5695fdd11ecc5d9dacb892c5c9
miguel <miguelpositivo02@gmail.com>
2023-05-16 22:15:26 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("a ginasta Rebeca Andrade"); 
+Console.WriteLine("digite a primeira nota");
+decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite a segunda nota");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+decimal notafinal = nota1 + nota2;
+ Console.WriteLine(notafinal);
+ 
